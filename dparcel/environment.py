72fe2d47835da9947dc7f13f1114910f67140a40
tschanzer <76029848+tschanzer@users.noreply.github.com>
2021-12-08 20:24:04 +1100

    Clean up environment.py

--- dparcel/environment.py
+++ dparcel/environment.py
@@ -1,6 +1,12 @@
-# Class for atmospheric sounding data
-# Thomas Schanzer, UNSW Sydney
-# September 2021
+# Copyright (c) 2021 Thomas Schanzer.
+# Distributed under the terms of the BSD 3-Clause License.
+"""Tools for specifying environmental profiles in dparcel.
+
+The Environment class allows the user to supply real atmospheric
+sounding data to use for parcel calculations. Alternatively, the
+idealised_sounding function can be used to generate an Environment
+instance using an idealised sounding.
+"""
 
 import numpy as np
 
@@ -41,12 +47,14 @@ class Environment:
             name: Short name for the sounding, e.g. 'Sydney' (optional).
         """
 
+        # record input data as attributes for use by methods
         self._pressure_raw = pressure.to(units.mbar)
         self._height_raw = height.to(units.meter)
         self._height_raw -= np.min(self._height_raw)  # set z=0 at surface
         self._temperature_raw = temperature.to(units.celsius)
         self._dewpoint_raw = dewpoint.to(units.celsius)
 
+        # if no liquid ratio profile is given, assume it is zero
         if liquid_ratio is None:
             self._liquid_ratio_raw = (
                 np.zeros(self._pressure_raw.size)*units.dimensionless)
@@ -58,6 +66,9 @@ class Environment:
 
         self.info = info
         self.name = name
+        
+        # functions to interpolate input data, so variables are known
+        # at any height
 
         self._p_to_T_interp = interp1d(
             self._pressure_raw.m, self._temperature_raw.m,
@@ -88,9 +99,7 @@ class Environment:
             fill_value='extrapolate')
 
     def temperature_from_pressure(self, pressure):
-        """
-        Finds the environmental temperature at a given pressure.
-        """
+        """Finds the environmental temperature at a given pressure."""
 
         temperature = self._p_to_T_interp(pressure.m_as(units.mbar))
         if temperature.size == 1:
@@ -98,9 +107,7 @@ class Environment:
         return temperature*units.celsius
 
     def dewpoint_from_pressure(self, pressure):
-        """
-        Finds the environmental dew point at a given pressure.
-        """
+        """Finds the environmental dew point at a given pressure."""
 
         dewpoint = self._p_to_Td_interp(pressure.m_as(units.mbar))
         if dewpoint.size == 1:
@@ -108,19 +115,15 @@ class Environment:
         return dewpoint*units.celsius
 
     def liquid_ratio_from_pressure(self, pressure):
-        """
-        Finds the environmental liquid ratio at a given pressure.
-        """
+        """Finds the environmental liquid ratio at a given pressure."""
 
-        l = self._p_to_l_interp(pressure.m_as(units.mbar))
-        if l.size == 1:
-            l = l.item()
-        return l*units.dimensionless
+        liquid_ratio = self._p_to_l_interp(pressure.m_as(units.mbar))
+        if liquid_ratio.size == 1:
+            liquid_ratio = liquid_ratio.item()
+        return liquid_ratio*units.dimensionless
 
     def pressure(self, height):
-        """
-        Finds the environmental pressure at a given height.
-        """
+        """Finds the environmental pressure at a given height."""
 
         pressure = self._z_to_p_interp(height.m_as(units.meter))
         if pressure.size == 1:
@@ -128,9 +131,7 @@ class Environment:
         return pressure*units.mbar
 
     def height(self, pressure):
-        """
-        Finds the height at a given environmental pressure.
-        """
+        """Finds the height at a given environmental pressure."""
 
         height = self._p_to_z_interp(pressure.m_as(units.mbar))
         if height.size == 1:
@@ -138,9 +139,7 @@ class Environment:
         return height*units.meter
 
     def temperature(self, height):
-        """
-        Finds the environmental temperature at a given height.
-        """
+        """Finds the environmental temperature at a given height."""
 
         temperature = self._z_to_T_interp(height.m_as(units.meter))
         if temperature.size == 1:
@@ -148,9 +147,7 @@ class Environment:
         return temperature*units.celsius
 
     def dewpoint(self, height):
-        """
-        Finds the environmental dew point at a given height.
-        """
+        """Finds the environmental dew point at a given height."""
 
         dewpoint = self._z_to_Td_interp(height.m_as(units.meter))
         if dewpoint.size == 1:
@@ -158,18 +155,20 @@ class Environment:
         return dewpoint*units.celsius
 
     def liquid_ratio(self, height):
-        """
-        Finds the environmental liquid ratio at a given height.
-        """
+        """Finds the environmental liquid ratio at a given height."""
 
-        l = self._z_to_l_interp(height.m_as(units.meter))
-        if l.size == 1:
-            l = l.item()
-        return l*units.dimensionless
+        liquid_ratio = self._z_to_l_interp(height.m_as(units.meter))
+        if liquid_ratio.size == 1:
+            liquid_ratio = liquid_ratio.item()
+        return liquid_ratio*units.dimensionless
 
     def wetbulb_temperature(self, height):
         """
         Finds the environmental wet-bulb temperature at a given height.
+        
+        Uses the approximation of eq. (39) in Bolton (1980) for
+        equivalent potential temperature, and the approximation of
+        Davies-Jones (2008) to find the wet bulb temperature.
         """
 
         pressure = self.pressure(height)
@@ -181,18 +180,14 @@ class Environment:
         return wetbulb(pressure, ept, improve=True)
 
     def specific_humidity(self, height):
-        """
-        Finds the environmental specific humidity at a given height.
-        """
+        """Finds the environmental specific humidity at a given height."""
 
         pressure = self.pressure(height)
         dewpoint = self.dewpoint(height)
         return mpcalc.specific_humidity_from_dewpoint(pressure, dewpoint)
 
     def density(self, height):
-        """
-        Finds the environmental density at a given height.
-        """
+        """Finds the environmental density at a given height."""
 
         pressure = self.pressure(height)
         temperature = self.temperature(height)
@@ -205,6 +200,12 @@ def idealised_sounding(relative_humidity, info='', name=''):
     """
     Creates an idealised sounding.
     
+    The sounding has a 160 mbar thick boundary layer with a dry
+    adiabatic temperature profile, a 10 mbar thick capping inversion
+    and a moist adiabatic temperature profile above the boundary layer.
+    The specific humidity is constant in the boundary layer, and the
+    relative humidity is constant above the boundary layer.
+    
     Args:
         relative_humidity: Relative humidity above the boundary layer.
         info: Information to store with the sounding, e.g. date
@@ -215,7 +216,7 @@ def idealised_sounding(relative_humidity, info='', name=''):
         An Environment instance.
     """
     
-    # generature temperature profile
+    # generate discrete temperature profile
     pressure = np.arange(1013.25, 200, -5)*units.mbar
     t_boundary_layer = mpcalc.dry_lapse(
         np.arange(1013.25, 1013.25 - 161, -5)*units.mbar, 20*units.celsius)
@@ -227,7 +228,7 @@ def idealised_sounding(relative_humidity, info='', name=''):
         t_boundary_layer, t_capping, t_remaining,
     ])
 
-    # generate dew point profile
+    # generate discrete dew point profile
     q_boundary_layer = mpcalc.specific_humidity_from_mixing_ratio(
         6e-3*units.dimensionless)
     dewpoint_boundary_layer = mpcalc.dewpoint_from_specific_humidity(
@@ -235,6 +236,7 @@ def idealised_sounding(relative_humidity, info='', name=''):
         np.ones(t_boundary_layer.size)*q_boundary_layer)
     dewpoint_remaining = mpcalc.dewpoint_from_relative_humidity(
         t_remaining, np.ones(t_remaining.size)*relative_humidity)
+    # ensure the dewpoint is continuous across the capping inversion
     dewpoint_capping_top = mpcalc.dewpoint_from_relative_humidity(
         t_capping[-1], relative_humidity)
     dewpoint_capping = concatenate([
@@ -246,6 +248,7 @@ def idealised_sounding(relative_humidity, info='', name=''):
         dewpoint_boundary_layer, dewpoint_capping, dewpoint_remaining,
     ])
     
+    # interpolate discrete profiles to give variables at any pressure
     temperature_interp = interp1d(
         pressure.m_as(units.pascal), temperature.m_as(units.kelvin),
         fill_value='extrapolate')
@@ -253,6 +256,8 @@ def idealised_sounding(relative_humidity, info='', name=''):
         pressure.m_as(units.pascal), dewpoint.m_as(units.kelvin),
         fill_value='extrapolate')
     
+    # now solve the hydrostatic equation so the variables can be
+    # expressed as functions of height
     def dzdp(pressure, height):
         """
         Calculates the rate of height change w.r.t. pressure, dz/dp.
